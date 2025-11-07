e68972a0801cd3dff7d48ef94a2e7004af074e69
faienz93 <afaienza93@gmail.com>
2025-11-07 09:24:42 +0100

    feat: added exercise 2 with chatbot

--- anthropic_api_fundamentals/02.01_few_show_prompting_chatbot.py
+++ anthropic_api_fundamentals/02.01_few_show_prompting_chatbot.py
@@ -1,27 +1,3 @@
-from dotenv import load_dotenv
-from anthropic import Anthropic
-
-# load environment variable
-load_dotenv()
-
-# automatically looks for an "ANTHROPIC_API_KEY" environment variable
-client = Anthropic()
-
-
-def translate(word, language):
-    response = client.messages.create(
-        model="claude-3-haiku-20240307",
-        max_tokens=1000,
-        messages=[
-            {
-                "role": "user",
-                "content": f"Translate ${word} to {language}. Respond with a single word",
-            }
-        ],
-    )
-    print(response.content[0].text)
-
-
 # Build a simple multi-turn command-line chatbot script. The messages format lends itself to building chat-based applications.  To build a chat-bot with Claude, it's as simple as:
 
 
@@ -31,15 +7,53 @@ def translate(word, language):
 # 4. Print out Claude's response to the user
 # 5. Add Claude's assistant response to the history
 # 6. Go back to step 2 and repeat! (use a loop and provide a way for users to quit!)
+
+from dotenv import load_dotenv
+from anthropic import Anthropic
+
+# load environment variable
+load_dotenv()
+
+# automatically looks for an "ANTHROPIC_API_KEY" environment variable
+client = Anthropic()
+conversation = []
+
+
+def chatbot():
+    response = client.messages.create(
+        model="claude-3-haiku-20240307",
+        max_tokens=1000,
+        messages=conversation,
+    )
+    claudeResponse = {
+        "role": response.role,
+        "content": response.content[0].text,
+    }
+    # print(response)
+    conversation.append(claudeResponse)
+    print(response.content[0].text)
+
+
 def main():
+
     print("Start Conversation Claude")
-    print("Digita un Nome o premi 'q' per uscire")
     while True:
-        userInput = input("Please enter your name: ")
+        userInput = input("Chiedimi qualcosa oppure premi 'q' per uscire: ")
+        if userInput:
+            userQuestion = {
+                "role": "user",
+                "content": userInput,
+            }
+            conversation.append(userQuestion)
+            chatbot()
         if userInput == "q":
             break
         print(f"Hello, {userInput}")
     print("!! Grazie per aver usato Claude !!")
+    if conversation:
+        print(conversation)
+    else:
+        print("Non cè storico delle conversazioni")
 
 
 if __name__ == "__main__":
